0e6e91d7b007d600623737934989d76a7415b1e5
nuumenyajelapki <33455940+nuumenyajelapki@users.noreply.github.com>
2019-01-21 22:09:14 +0300

    Add files via upload

---
+++ lab4-5/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'main'
+urlpatterns = [
+
+    path('<int:id>/', views.detail, name='detail'),
+    path('', views.list, name='listView')
+
+]
